55fbff31599b97e089b73a0f7b4b4e0b4d40cb80
HKhilji <122511346+HKhilji@users.noreply.github.com>
2023-11-18 20:50:21 +0500

    Update main.cpp
    Added functionality after building the main window that will display lives, the map itself, and the current score. Rest will be done later.

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,17 @@
 
 int SDL_main(int argv, char* argc[]) {
 
-    IMG_Init(IMG_INIT_PNG);
-
-    int screenWidth = 672;
-    int screenHeight = 888;
+    int screenWidth = 766;
+    int screenHeight = 1000;
 
     Game game("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
 
-    SDL_Texture* map = game.loadTexture("res/gfx/Map24.png");
+    SDL_Texture* map = game.loadTexture("res/gfx/map.png");
+    SDL_Texture* lives = game.loadTexture("res/gfx/Lives32.png");
 
     while (game._gameState != GameState::EXIT) {
-        SDL_Event event;
-        SDL_PollEvent(&event);
-
-        switch (event.type) {
-            case SDL_QUIT:
-                game._gameState = GameState::EXIT;
-                break;
-        }
-
-        game.run(map);
+        game.run(map, 1, 0, 90, screenWidth, 849, lives, 0, 23, 962, 32, 32, lives, 0, 60, 962, 32, 32, lives, 0, 97, 962, 32, 32);
     }
 
-    IMG_Quit();
     return 0;
 }
