f7c83c0252dcec7bbe70c2c0d7f2af9ddd2916dd
Zain Hatim <75770574+ZainHatimM@users.noreply.github.com>
2023-12-03 22:44:21 +0500

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,34 @@
+#include "player.h"
+#include "grid.h"
+
+void Player::Move(Grid& grid)
+{
+  //acquire gird mutex
+  std::lock_guard<std::mutex> lck(grid.mtx);
+  //check for wall before moving
+  switch (direction)
+  {
+    case Direction::kUp:
+      y = (grid.at(x,y-1) == Grid::GridElements::kWall)? y : (y-1);
+      break;
+    case Direction::kDown:
+      y = (grid.at(x,y+1) == Grid::GridElements::kWall)? y : (y+1);
+      break;
+    case Direction::kLeft:
+      x = (grid.at(x-1,y) == Grid::GridElements::kWall)? x : (x-1);
+      break;
+    case Direction::kRight:
+      x = (grid.at(x+1,y) == Grid::GridElements::kWall)? x : (x+1);
+      break;
+  }
+  //check for portal
+  if(x == 0 && y == 10){ x = 17; }
+  if(x == 18 && y == 10){ x = 1; }
+  //check for food and eat it
+  if(grid.at(x,y) == Grid::GridElement::kFood)
+  {
+    score++;
+    grid.set(x,y,Grid::GridElement::kEmpty);
+  }
+}
+
