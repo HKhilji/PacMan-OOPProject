af57985c1e5892446fa452397ff90dc7427a5ca4
Reckon1ng <ahmedkhalid12009@gmail.com>
2023-12-04 16:40:21 +0500

    dumb syntax error, mb

--- controller.cpp
+++ controller.cpp
@@ -1,11 +1,11 @@
 #include "SDL.h"
 #include "controller.h"
 
-void Controller::handleinputanddirection(bool& running, Player& player, Grid& grid){
+void Controller::handleinputanddirection(bool& moving, Player& player, Grid& grid){
     SDL_Event event; //this handles the events, reading and placing events on queue
     while (SDL_PollEvent(&event)) { //checking to pass info into the event.
-        if (event.type == SDL.QUIT) { //sets running false if event is to quit
-            running = false;
+        if (event.type == SDL.QUIT) { //sets moving false if event is to quit
+            moving = false;
         }
         //this is for key detection, keydown is for detecting when key is pressed
         if (event.type == SDL_KEYDOWN) {
