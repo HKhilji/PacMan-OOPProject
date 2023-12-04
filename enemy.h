5153259e8cacd935adb2d4e16656c7769e5ddfe6
Reckon1ng <ahmedkhalid12009@gmail.com>
2023-12-04 17:50:11 +0500

    blueghost should be valid now

--- enemy.h
+++ enemy.h
@@ -8,15 +8,21 @@
 class ghost{
     public:
         ghost(int a, int b): x(a), y(b) {}
-        int x, y;
+        int x, y; //positions
+        virtual void Ghost_Move(Grid& grid, Player& player, std::promise<void>&& prms) = 0;
 }
 
 //random movement ghost
 class BlueGhost{
     public:
         BlueGhost(int a, int b): ghost(a,b) {}
+        void Ghost_Move(Grid& grid, Player& player, std::promise<void>&&prms) override;
     private:
-}
+        int prev_x = 0;
+        int prev_y = 0;
+        void RandomAlgo(Grid& grid);
+        bool random_valid_cell(int x, int y, Grid& grid);
+};
 
 //searches for player
 class RedGhost{
