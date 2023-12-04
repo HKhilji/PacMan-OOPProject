1f571b40663364dc6a2ffc65b1ac4fee650c3853
HKhilji <ha08041@st.habib.edu.pk>
2023-12-04 20:21:47 +0500

    fixed typos and made definition of destructor. Also added freesurface as it is essential for it to render

--- GameRender.cpp
+++ GameRender.cpp
@@ -4,6 +4,16 @@
 GameRender::GameRender(){
     sdl_window = SDL_CreateWindow("Pacman!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Surface* iamge = SDL_LoadBMP("../res/gfx/pacman50x50spritesheet.bmp");
+    SDL_Surface* image = SDL_LoadBMP("../res/gfx/pacman50x50spritesheet.bmp");
     tex = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_FreeSurface(image);
+    image = nullptr;
+}
+
+GameRender::~GameRender(){
+    SDL_DestroyTexture(tex);
+    tex = nullptr;
+
+    SDL_DestroyWindow(sdl_window);
+    sdl_window = nullptr;
 }
