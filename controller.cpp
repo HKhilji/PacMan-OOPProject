d37a36f2caa376c1463723a2f9a6c89cbb8d1be4
Reckon1ng <ahmedkhalid12009@gmail.com>
2023-12-04 16:19:55 +0500

    condition for stopping running done

--- controller.cpp
+++ controller.cpp
@@ -2,5 +2,10 @@
 #include "controller.h"
 
 void Controller::handleinputanddirection(bool& running, Player& player, Grid& grid){
-    SDL_Event event;
+    SDL_Event event; //this handles the events, reading and placing events on queue
+    while (SDL_PollEvent(&event)) { //checking to pass info into the event.
+    if (event.type == SDL.QUIT) { //sets running false if event is to quit
+        running = false;
+    }
+    }
 }
