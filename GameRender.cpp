4a7d0257badd6652e0e83f50e8ff1d6289c69508
HKhilji <ha08041@st.habib.edu.pk>
2023-12-04 20:19:52 +0500

    updated constructor definition, will test everything out after it's done.

--- GameRender.cpp
+++ GameRender.cpp
@@ -4,5 +4,6 @@
 GameRender::GameRender(){
     sdl_window = SDL_CreateWindow("Pacman!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
-    
+    SDL_Surface* iamge = SDL_LoadBMP("../res/gfx/pacman50x50spritesheet.bmp");
+    tex = SDL_CreateTextureFromSurface(renderer, image);
 }
