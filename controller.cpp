e7ae26683135d2ee839066d336713dc216d4e42f
Reckon1ng <ahmedkhalid12009@gmail.com>
2023-12-04 16:35:38 +0500

    shifted sum stuff aroudn for readability

--- controller.cpp
+++ controller.cpp
@@ -4,17 +4,17 @@
 void Controller::handleinputanddirection(bool& running, Player& player, Grid& grid){
     SDL_Event event; //this handles the events, reading and placing events on queue
     while (SDL_PollEvent(&event)) { //checking to pass info into the event.
-    if (event.type == SDL.QUIT) { //sets running false if event is to quit
-        running = false;
-    }
-    //this is for key detection, keydown is for detecting when key is pressed
-    if (event.type == SDL_KEYDOWN) {
-        switch(event.key.keysm.sym) {
-            case SDLK_UP: //this if condition is just to check if collisison with wall. uses grid.cpp
-					player.direction = (grid.at(player.x, player.y-1) == Grid::GridElement::kWall)? player.direction : Player::Direction::kUp;
-					break;
-
+        if (event.type == SDL.QUIT) { //sets running false if event is to quit
+            running = false;
         }
-    }
+        //this is for key detection, keydown is for detecting when key is pressed
+        if (event.type == SDL_KEYDOWN) {
+            switch(event.key.keysm.sym) {
+                case SDLK_UP: //this if condition is just to check if collisison with wall. uses grid.cpp
+                        player.direction = (grid.at(player.x, player.y-1) == Grid::GridElement::kWall)? player.direction : Player::Direction::kUp;
+                        break;
+
+            }
+        }
     }
 }
