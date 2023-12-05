03f66cb9cb118d0d968939c12cf6f3cab6969330
HKhilji <ha08041@st.habib.edu.pk>
2023-12-05 15:52:22 +0500

    I am so mad at zain. Finally fixed the map rendering as the grid.cpp was returning [y][x] instead of [x][y]

--- grid.cpp
+++ grid.cpp
@@ -9,29 +9,29 @@ Grid::Grid()
 
 void Grid::LoadGameGrid()
 {
-  int i = 0;
-  std::ifstream ifs("C:/Users/DELL/Documents/GitHub/PacMan-OOPProject/res/gfx/grid.txt");
-  if(!ifs)
-  { 
-    std::cerr << "Failed to open file <../res/gfx/grid.txt>\n";
+    int i = 0;
+    std::ifstream ifs("C:/Users/DELL/Documents/GitHub/PacMan-OOPProject/res/gfx/grid.txt");
+    if(!ifs)
+    { 
+      std::cerr << "Failed to open file <../res/gfx/grid.txt>\n";
     return;
-  };
-  std::string line;
-  while(std::getline(ifs,line))
-  {
-      for(int j = 0; j < 19; j++)
-      {
-        grid[i][j] = line[j] - '0';
-      }
-    i++;
-  }
+    };
+    std::string line;
+    while(std::getline(ifs,line))
+    {
+        for(int j = 0; j < 19; j++)
+        {
+          grid[i][j] = line[j] - '0';
+        }
+        i++;
+    }
 }
 
 void Grid::PrintGameGrid()
 {
   for(int i = 0; i < 23; i++)
   {
-    for(int j = 0; j < 29; j++)
+    for(int j = 0; j < 19; j++)
     {
       std::cout << grid[i][j];
     }
@@ -41,7 +41,7 @@ void Grid::PrintGameGrid()
 
 int Grid::at(int x, int y)
 {
-  return grid[x][y];
+  return grid[y][x];
 }
 
 void Grid::set(int x, int y, int val)
